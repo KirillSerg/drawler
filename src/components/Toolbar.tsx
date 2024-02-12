8763e36094fb1279fd3a2c3ab36316294d8238f7
KirillSerg <kirill.sergienko1989@gmail.com>
2024-02-12 02:08:14 +0200

    feat: re-write handling events with Jotai

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,23 +1,23 @@
 import { useAtom } from 'jotai';
-import { addElementsAtom } from '../store/store';
-
-const emptyElement = {
-  id: '',
-  x: 960,
-  y: 0,
-  width: 240,
-  height: 300,
-  stroke: 'black',
-  strokeWidth: 8,
-  fill: 'none',
-};
+import { activeElementTypeAtom } from '../store/store';
 
 const Toolbar = () => {
-  const [, addElements] = useAtom(addElementsAtom);
+  const [activeElementType, setActiveElementType] = useAtom(
+    activeElementTypeAtom,
+  );
 
   return (
     <header className="h-[6%] sticky top-0 flex justify-center gap-4 border-4 border-black">
-      <button onClick={() => addElements({ ...emptyElement, type: 'rect' })}>
+      <button
+        className={`${activeElementType === 'free' ? 'bg-orange-500' : 'bg-inherit'}`}
+        onClick={() => setActiveElementType('free')}
+      >
+        🖱️
+      </button>
+      <button
+        className={`${activeElementType === 'rect' ? 'bg-orange-500' : 'bg-inherit'}`}
+        onClick={() => setActiveElementType('rect')}
+      >
         <svg
           viewBox="0 0 24 24"
           height="50%"
@@ -34,10 +34,18 @@ const Toolbar = () => {
           />
         </svg>
       </button>
-
-      <button className="border border-blue-700">elips</button>
-
-      <button className="border border-blue-700">line</button>
+      <button
+        className={`${activeElementType === 'ellipse' ? 'bg-orange-500' : 'bg-inherit'}`}
+        onClick={() => setActiveElementType('ellipse')}
+      >
+        elips
+      </button>
+      <button
+        className={`${activeElementType === 'line' ? 'bg-orange-500' : 'bg-inherit'}`}
+        onClick={() => setActiveElementType('line')}
+      >
+        line
+      </button>
     </header>
   );
 };
