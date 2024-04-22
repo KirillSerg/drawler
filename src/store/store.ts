b6d65de40c1ada9e98599c81cb21b5a1eedc7f8e
KirillSerg <kirill.sergienko1989@gmail.com>
2024-04-22 22:14:37 +0300

    fix: fix dragging when creating; add small refact

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import { atom } from "jotai";
 import { Area, Coordinates, Element } from "../types/Common";
-import { atomWithImmer } from "jotai-immer";
+import { atomWithStorage } from 'jotai/utils'
 
 const initialElement: Element = {
   type: "rect",
@@ -14,32 +14,29 @@ const initialElement: Element = {
   fill: 'none',
 }
 
-export const activeElementTypeAtom = atom<Element["type"]>("free")
-
-export const elementsAtom = atomWithImmer<Element[]>([])
-
+export const creatingElementTypeAtom = atom<Element["type"]>("free")
+export const elementsAtom = atomWithStorage<Element[]>("elements", [])
 export const selectedElementAtom = atom<Element | null>(null)
-
 export const selectingAreaAtom = atom<Area | null>(null)
-
+export const isDraggingAtom = atom(false)
 export const isDrawingAtom = atom(
-  (get) => get(activeElementTypeAtom) === "free" ? false : true
+  (get) => get(creatingElementTypeAtom) === "free" ? false : true
 )
 
 export const updateElementsAtom = atom(
   null,
   (_get, set, updatedElement: Element) => {
+    // console.log("updateElementsAtom")
     set(elementsAtom, (prev) => prev.map((el) =>
       el.id === updatedElement.id ? updatedElement : el,
     ))
   }
 )
 
-export const isDraggingAtom = atom(false)
-
 export const onMouseDownAtom = atom(
   null,
   (get, set, update: Coordinates) => {
+    console.log("onMouseDownAtom")
     if (!get(isDraggingAtom)) {
       set(selectedElementAtom, null)
     }
@@ -54,27 +51,25 @@ export const onMouseDownAtom = atom(
 
 export const onDragStartAtom = atom(
   null,
-  (_get, set, update: { element: Element, position: Coordinates }) => {
-    set(selectedElementAtom, update.element)
-    set(isDraggingAtom, true)
-    set(selectingAreaAtom, {
-      startX: update.position.x,
-      startY: update.position.y,
-      endX: 0,
-      endY: 0
-    })
+  (get, set, update: { element: Element, position: Coordinates }) => {
+    console.log("onDragStartAtom")
+    if (!get(isDrawingAtom)) {
+      set(selectedElementAtom, update.element)
+      set(isDraggingAtom, true)
+    }
   }
 )
 
 export const onMouseMoveAtom = atom(
   null,
   (get, set, update: Coordinates) => {
+    console.log("onMouseMoveAtom")
     const selectingArea = get(selectingAreaAtom)
     if (selectingArea) {
       const selectedElement = get(selectedElementAtom)
-      if (get(isDraggingAtom) && selectingArea && selectedElement?.x && selectedElement.y) {
-        const newX = update.x - selectingArea.startX + selectedElement.x
-        const newY = update.y - selectingArea.startY + selectedElement.y
+      if (get(isDraggingAtom) && selectedElement?.x && selectedElement.y) {
+        const newX = selectedElement.x + (update.x - selectingArea.startX)
+        const newY = selectedElement.y + (update.y - selectingArea.startY)
         set(updateElementsAtom, { ...selectedElement, x: newX, y: newY })
       }
       set(selectingAreaAtom, { ...selectingArea, endX: update.x, endY: update.y })
@@ -85,13 +80,14 @@ export const onMouseMoveAtom = atom(
 export const onMouseUpAtom = atom(
   null,
   (get, set) => {
+    console.log("onMouseUpAtom")
     if (get(isDrawingAtom)) {
-      const activeElementType = get(activeElementTypeAtom)
-      const newEl = { ...initialElement, type: activeElementType, id: crypto.randomUUID() }
+      const creatingElementType = get(creatingElementTypeAtom)
+      const newEl = { ...initialElement, type: creatingElementType, id: crypto.randomUUID() }
       set(elementsAtom, (prev) => [...prev, newEl])
       set(selectedElementAtom, newEl)
+      set(creatingElementTypeAtom, "free")
     }
-    set(activeElementTypeAtom, "free")
     set(isDraggingAtom, false)
     set(selectingAreaAtom, null)
   }
