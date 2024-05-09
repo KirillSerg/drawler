6674bf3a0409514d1a807d9946d19109a77f7b00
KirillSerg <kirill.sergienko1989@gmail.com>
2024-05-09 21:50:48 +0300

    rename worflows folder)

--- playwright.config.ts
+++ playwright.config.ts
@@ -69,9 +69,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:5173/',
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: 'npm run dev',
+  //   url: 'http://localhost:5173/',
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
