9ef6c819b9d29ceb96e2e7b5a1649d97756a23ca
KirillSerg <kirill.sergienko1989@gmail.com>
2024-05-20 21:38:28 +0300

    feat: added arrow-line to the toolbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -24,10 +24,10 @@ const Toolbar = () => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <rect
-            x="0"
-            y="0"
-            width="100%"
-            height="100%"
+            x="2"
+            y="2"
+            width="20"
+            height="20"
             stroke="black"
             strokeWidth="2"
             fill="none"
@@ -44,12 +44,12 @@ const Toolbar = () => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <ellipse
-            cx="50%"
-            cy="50%"
-            rx="45%"
-            ry="35%"
+            cx="12"
+            cy="12"
+            rx="10"
+            ry="7"
             stroke="black"
-            strokeWidth="10%"
+            strokeWidth="2"
             fill="none"
           />
         </svg>
@@ -65,7 +65,7 @@ const Toolbar = () => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <polygon
-            points="12,0 24,24 0,24"
+            points="12,2 22,22 2,22"
             fill="none"
             stroke="black"
             strokeWidth="2"
@@ -89,6 +89,39 @@ const Toolbar = () => {
             y2="50%"
             stroke="black"
             strokeWidth="2"
+          />
+        </svg>
+      </button>
+
+      <button
+        className={`${creatingElementType === 'line' ? 'bg-orange-500' : 'bg-inherit'} p-[6px]`}
+        onClick={() => setCreatingElementType('line')}
+      >
+        <svg
+          viewBox="0 0 24 24"
+          height="100%"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <marker
+              id="arrow"
+              markerWidth="5"
+              markerHeight="5"
+              refX="5"
+              refY="2.5"
+              orient="auto"
+            >
+              <path d="M 0 0 L 5 2.5 L 0 5" fill="none" stroke="black" />
+            </marker>
+          </defs>
+          <line
+            x1="0"
+            y1="12"
+            x2="24"
+            y2="12"
+            stroke="black"
+            strokeWidth="2"
+            marker-end="url(#arrow)"
           />
         </svg>
       </button>
