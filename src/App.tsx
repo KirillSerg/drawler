96a8f29fe6e145511a50974dc28db474965f3db8
KirillSerg <kirill.sergienko1989@gmail.com>
2024-07-14 11:13:13 +0300

    feat: added inspector styles

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
       <Toolbar />
       <Canvas />
       <Inspector />
-      <Layers />
+      {/* <Layers /> */}
       <Zoom />
     </div>
   );
--- src/components/Inspector.tsx
+++ src/components/Inspector.tsx
@@ -68,52 +68,64 @@ const Inspector = () => {
   );
 
   return (
-    <aside className="fixed min-w-[10%] max-w-[25%] max-h-[80%] overflow-auto px-3 py-5  top-[10%] right-5 border border-black">
-      Inspector
+    <>
       {elements[0].id && (
-        <img
-          onClick={deleteElements}
-          src={deleteIcon}
-          alt="delete"
-          width={24}
-          height={24}
-        />
+        <aside className="fixed min-w-[10%] max-w-[25%] max-h-[80%] overflow-auto px-3 py-5 top-[10%] left-5 border border-black">
+          <>
+            <p>Actions</p>
+            <div id="actions" className="flex gap-x-1">
+              <img
+                onClick={deleteElements}
+                src={deleteIcon}
+                alt="delete"
+                width={24}
+                height={24}
+              />
+            </div>
+          </>
+          {/* Line marker */}
+          {(isArrow_line || isLine) && (
+            <>
+              <p>Linehead</p>
+              <div id="linehead" className="flex gap-x-1">
+                <LineProp
+                  className={`${
+                    isLine ? 'bg-blue-300' : 'bg-gray-200'
+                  } h-8 w-8 p-[6px] rounded-md`}
+                  handlerClick={handlerSelectProperty}
+                />
+                <LineArrowProp
+                  className={`${
+                    isArrow_line ? 'bg-blue-300' : 'bg-gray-200'
+                  } h-8 w-8 p-[6px] rounded-md`}
+                  handlerClick={handlerSelectProperty}
+                />
+              </div>
+            </>
+          )}
+          {/* Edges propertys */}
+          {(isEdgesRound || isEdgesSharp) && (
+            <>
+              <p>Edges</p>
+              <div id="edges" className="flex gap-x-1">
+                <EdgeRoundProp
+                  className={`${
+                    isEdgesRound ? 'bg-blue-300' : 'bg-gray-200'
+                  } h-8 w-8 p-[6px] rounded-md`}
+                  handlerClick={handlerSelectProperty}
+                />
+                <EdgeSharpProp
+                  className={`${
+                    isEdgesSharp ? 'bg-blue-300' : 'bg-gray-200'
+                  } h-8 w-8 p-[6px] rounded-md`}
+                  handlerClick={handlerSelectProperty}
+                />
+              </div>
+            </>
+          )}
+        </aside>
       )}
-      {/* Line marker */}
-      {(isArrow_line || isLine) && (
-        <>
-          <LineProp
-            className={`${
-              isLine ? 'bg-orange-500' : 'bg-inherit'
-            } h-8 w-8 p-[6px]`}
-            handlerClick={handlerSelectProperty}
-          />
-          <LineArrowProp
-            className={`${
-              isArrow_line ? 'bg-orange-500' : 'bg-inherit'
-            } h-8 w-8 p-[6px]`}
-            handlerClick={handlerSelectProperty}
-          />
-        </>
-      )}
-      {/* Edges propertys */}
-      {(isEdgesRound || isEdgesSharp) && (
-        <>
-          <EdgeRoundProp
-            className={`${
-              isEdgesRound ? 'bg-orange-500' : 'bg-inherit'
-            } h-6 w-6`}
-            handlerClick={handlerSelectProperty}
-          />
-          <EdgeSharpProp
-            className={`${
-              isEdgesSharp ? 'bg-orange-500' : 'bg-inherit'
-            } h-6 w-6`}
-            handlerClick={handlerSelectProperty}
-          />
-        </>
-      )}
-    </aside>
+    </>
   );
 };
 
