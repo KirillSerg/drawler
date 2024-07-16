a1b3babaf7ce843e6d71aa6f244b495e2b4a4f37
KirillSerg <kirill.sergienko1989@gmail.com>
2024-07-16 16:46:12 +0300

    fix: small fix

--- src/components/inspectorElements/ColorsPalette.tsx
+++ src/components/inspectorElements/ColorsPalette.tsx
@@ -1,5 +1,6 @@
 import { colorsPalette } from '../../assets/utilities';
 import { ElementProps } from '../../types/CommonTypes';
+import transparentFone from '../../assets/icons/transparentBG.png';
 
 interface Props {
   targetProp: string;
@@ -14,17 +15,14 @@ const ColorsPalette = ({ targetProp, handlerClick, activeColor }: Props) => {
         <button
           key={color}
           className={`w-5 h-5 rounded-sm ${activeColor === color ? 'border-2 border-dotted' : ''}`}
-          style={{ backgroundColor: `${color}` }}
+          style={{
+            backgroundColor: `${color}`,
+            backgroundImage: `${color === 'transparent' ? `url(${transparentFone})` : ''}`,
+            backgroundSize: '12rem',
+          }}
           onClick={() =>
             handlerClick({
-              types: [
-                'rect',
-                'ellipse',
-                'line',
-                'polygon',
-                'foreignObject',
-                'path',
-              ],
+              types: ['rect', 'ellipse', 'line', 'polygon', 'foreignObject'],
               [targetProp]: color,
             })
           }
